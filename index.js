43f4499eac5909c6fb8fd631b378d8abaee002af
AbuBakrKhan24 <abubakrkhan224@gmail.com>
2022-06-23 16:31:14 +0200

    did alots

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+// localStorage.removeItem('records');
 // Adding array
-const items = [
-  {
-    id: 1,
-    product: "TV Stand",
-    createDate: new Date(),
-  },
-];
+let items = JSON.parse(localStorage.getItem("Stored_Info"))
+  ? JSON.parse(localStorage.getItem("Stored_Info"))
+  : [
+      {
+        id: 1,
+        product: "TV Stand",
+        createDate: new Date(),
+      },
+    ];
 
 // Original Loop
-
-items.forEach((item) => {
-  console.log(item);
-  document.getElementById("list").innerHTML += `
-  <div class="item" id="listing"><input class="chb checkboxes"  type="checkbox"  ><label for="add_info">${item.product}</label></div>
+function showItems(items) {
+  document.querySelector("#list").innerHTML = "";
+  items.forEach((item) => {
+    console.log(item);
+    document.getElementById("list").innerHTML += `
+  <div class="item" id="listing"><input class="chb checkboxes"  type="checkbox"  ><label for="add_info">${item.product}</label><i class="fa-solid fa-trash-can"></i></div>
   `;
-});
+  });
+}
+showItems(items);
 
 // Add Function
 function add() {
@@ -29,4 +35,6 @@ function add() {
   `;
 
   console.log(items);
+  localStorage.setItem("Stored_Info", JSON.stringify(items));
+  showItems(items);
 }
